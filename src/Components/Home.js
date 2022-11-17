7e9c351c683aecf9aa4b4f118c439be3b39a14b1
Camilo Cuitiva <camiloandres174@gmail.com>
2022-11-16 23:17:43 -0500

    style: add placeholder's in the register

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -49,7 +49,7 @@ function Home() {
                     <p className="text-center mb-5 text-nile-blue-600">Enter your email to get started</p>
                     <div className="flex flex-col mb-3">
                         <label htmlFor="email" className="pb-1 text-nile-blue-600">Email</label>
-                        <input type="email" name="email" id="email" placeholder="ejemplo@ejemplo.com" className="bg-nile-blue-50 border border-nile-blue-500 text-sm rounded-lg focus:ring-nile-blue-500 focus:border-nile-blue-700 block w-full p-2.5"/>
+                        <input type="email" name="email" id="email" placeholder="example@example.com" className="bg-nile-blue-50 border border-nile-blue-500 text-sm rounded-lg focus:ring-nile-blue-500 focus:border-nile-blue-700 block w-full p-2.5"/>
                     </div>
                     <div className="flex flex-col mb-3">
                         <label htmlFor="password" className="pb-1 text-nile-blue-600">Password</label>
@@ -65,15 +65,15 @@ function Home() {
                     <h1 className="text-3xl font-bold text-center mb-5 text-nile-blue-900">Create a new account</h1>
                     <div className="flex flex-col mb-3">
                         <label htmlFor="name" className="pb-1 text-nile-blue-600">Name</label>
-                        <input type="text" name="name" id="name" placeholder="" className="bg-nile-blue-50 border border-nile-blue-500 text-sm rounded-lg focus:ring-nile-blue-500 focus:border-nile-blue-700 block w-full p-2.5"/>
+                        <input type="text" name="name" id="name" placeholder="Jhon Smith" className="bg-nile-blue-50 border border-nile-blue-500 text-sm rounded-lg focus:ring-nile-blue-500 focus:border-nile-blue-700 block w-full p-2.5"/>
                     </div>
                     <div className="flex flex-col mb-3">
                         <label htmlFor="email" className="pb-1 text-nile-blue-600">Email</label>
-                        <input type="email" name="email" id="email" placeholder="" className="bg-nile-blue-50 border border-nile-blue-500 text-sm rounded-lg focus:ring-nile-blue-500 focus:border-nile-blue-700 block w-full p-2.5"/>
+                        <input type="email" name="email" id="email" placeholder="example@example.com" className="bg-nile-blue-50 border border-nile-blue-500 text-sm rounded-lg focus:ring-nile-blue-500 focus:border-nile-blue-700 block w-full p-2.5"/>
                     </div>
                     <div className="flex flex-col mb-3">
                         <label htmlFor="password" className="pb-1 text-nile-blue-600">Password</label>
-                        <input type="password" name="password" id="password" placeholder="" className="bg-nile-blue-50 border border-nile-blue-500 text-sm rounded-lg focus:ring-nile-blue-500 focus:border-nile-blue-700 block w-full p-2.5"/>
+                        <input type="password" name="password" id="password" placeholder="***********" className="bg-nile-blue-50 border border-nile-blue-500 text-sm rounded-lg focus:ring-nile-blue-500 focus:border-nile-blue-700 block w-full p-2.5"/>
                     </div>
                     <div className="flex justify-center my-5 ">
                         <button type="submit" className="w-full bg-burnt-sienna-500 hover:bg-burnt-sienna-500 py-2 px-3 text-nile-blue-50 rounded-lg text-center">Register</button>
