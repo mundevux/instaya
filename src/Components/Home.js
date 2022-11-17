6cb057d2fc1f05226b2d4fb2c70d1c9e09e7618d
Camilo Cuitiva <camiloandres174@gmail.com>
2022-11-16 21:28:22 -0500

    style: update styles

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -8,21 +8,22 @@ function Home() {
             <div className="mx-5">
                 <img src={Logo} alt="Logo InstaYa" className="h-36 mx-auto"/>
                 <form action="" className="">
-                    <h1 className="text-3xl font-bold text-center my-5">Iniciar Sesión</h1>
+                    <h1 className="text-3xl font-bold text-center my-5 text-nile-blue-900">Hello Again!</h1>
+                    <p className="text-center mb-5 text-nile-blue-600">Enter your email to get started</p>
                     <div className="flex flex-col mb-5">
-                        <label htmlFor="email" className="pb-1">Email</label>
+                        <label htmlFor="email" className="pb-1 text-nile-blue-600">Email</label>
                         <input type="email" name="email" id="email" placeholder="ejemplo@ejemplo.com" className="bg-nile-blue-50 border border-nile-blue-500 text-sm rounded-lg focus:ring-nile-blue-500 focus:border-nile-blue-700 block w-full p-2.5"/>
                     </div>
                     <div className="flex flex-col mb-5">
-                        <label htmlFor="password" className="pb-1">Password</label>
+                        <label htmlFor="password" className="pb-1 text-nile-blue-600">Password</label>
                         <input type="password" name="password" id="password" placeholder="***********" className="bg-nile-blue-50 border border-nile-blue-500 text-sm rounded-lg focus:ring-nile-blue-500 focus:border-nile-blue-700 block w-full p-2.5"/>
                     </div>  
                     <div className="flex justify-center my-5 ">
                         <button type="submit" className="w-full bg-burnt-sienna-600 hover:bg-burnt-sienna-500 py-2 px-3 text-nile-blue-50 rounded-lg text-center">Login</button>
                     </div>
                     <div className="flex justify-around items-center mt-5">
-                        <a href="./#" className="mx-3 text-center">¿Olvidaste tu contraseña?</a>
-                        <a href="./#" className="mx-3 text-center">¿No tienes una cuenta?</a>
+                        <a href="./#" className="mx-3 text-center text-xs md:text-base text-burnt-sienna-500">¿Olvidaste tu contraseña?</a>
+                        <a href="./#" className="mx-3 text-center text-xs md:text-base text-burnt-sienna-500">¿No tienes una cuenta?</a>
                     </div>
                 </form>
             </div>
