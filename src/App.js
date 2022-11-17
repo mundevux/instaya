a30a706d0ed88095f7ea2efa9d473199c0986953
Camilo Cuitiva <camiloandres174@gmail.com>
2022-11-16 19:34:06 -0500

    style: Mobile view update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import Header from "./Components/Hearder";
+
 import Home from "./Components/Home";
 
 
 function App() {
   return (
     <div className="container mx-auto font-[Manrope]">
-      <Header />
       <Home />
     </div>
     
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,28 +1,32 @@
 import React from "react";
 import image1 from "../img/entrega-paquete.jpg";
+import Logo from '../img/Logo_InstaYa.png';
 
 function Home() {
     return (
-        <section className="flex justify-around items-center h-screen -mt-28">
-            <form action="" className="">
-                <h1 className="text-3xl font-bold text-center my-5">Iniciar Sesión</h1>
-                <div className="flex flex-col mb-5">
-                    <label htmlFor="email" className="pb-1">Email</label>
-                    <input type="email" name="email" id="email" placeholder="ejemplo@ejemplo.com" className="bg-nile-blue-50 border border-nile-blue-500 text-sm rounded-lg focus:ring-nile-blue-500 focus:border-nile-blue-700 block w-full p-2.5"/>
-                </div>
-                <div className="flex flex-col mb-5">
-                    <label htmlFor="password" className="pb-1">Password</label>
-                    <input type="password" name="password" id="password" placeholder="***********" className="bg-nile-blue-50 border border-nile-blue-500 text-sm rounded-lg focus:ring-nile-blue-500 focus:border-nile-blue-700 block w-full p-2.5"/>
-                </div>  
-                <div className="flex justify-center my-5 ">
-                    <button type="submit" className=" bg-burnt-sienna-600 hover:bg-burnt-sienna-500 py-2 px-3 text-nile-blue-50 rounded-lg text-center">Login</button>
-                </div>
-                <div className="flex justify-around items-center mt-5">
-                    <a href="./#" className="mx-3">¿Olvidaste tu contraseña?</a>
-                    <a href="./#" className="mx-3">¿No tienes una cuenta?</a>
-                </div>
-            </form>
-            <img src={image1} alt="Entrega de paquete" className="rounded-lg shadow-xl" />
+        <section className="flex justify-around items-center h-screen lg:container lg:mx-auto">
+            <div className="mx-5">
+                <img src={Logo} alt="Logo InstaYa" className="h-36 mx-auto"/>
+                <form action="" className="">
+                    <h1 className="text-3xl font-bold text-center my-5">Iniciar Sesión</h1>
+                    <div className="flex flex-col mb-5">
+                        <label htmlFor="email" className="pb-1">Email</label>
+                        <input type="email" name="email" id="email" placeholder="ejemplo@ejemplo.com" className="bg-nile-blue-50 border border-nile-blue-500 text-sm rounded-lg focus:ring-nile-blue-500 focus:border-nile-blue-700 block w-full p-2.5"/>
+                    </div>
+                    <div className="flex flex-col mb-5">
+                        <label htmlFor="password" className="pb-1">Password</label>
+                        <input type="password" name="password" id="password" placeholder="***********" className="bg-nile-blue-50 border border-nile-blue-500 text-sm rounded-lg focus:ring-nile-blue-500 focus:border-nile-blue-700 block w-full p-2.5"/>
+                    </div>  
+                    <div className="flex justify-center my-5 ">
+                        <button type="submit" className="w-full bg-burnt-sienna-600 hover:bg-burnt-sienna-500 py-2 px-3 text-nile-blue-50 rounded-lg text-center">Login</button>
+                    </div>
+                    <div className="flex justify-around items-center mt-5">
+                        <a href="./#" className="mx-3 text-center">¿Olvidaste tu contraseña?</a>
+                        <a href="./#" className="mx-3 text-center">¿No tienes una cuenta?</a>
+                    </div>
+                </form>
+            </div>
+            <img src={image1} alt="Entrega de paquete" className="hidden lg:block rounded-lg shadow-xl lg:w-2/4 xl:w-3/5 " />
         </section>
     )
 }
